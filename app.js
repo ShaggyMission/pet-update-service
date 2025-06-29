8ffa72f15608d44bbdfefbb673962d8b9b374cc5
Lis Pacheco <lmpacheco@uce.edu.ec>
2025-06-29 11:05:48 -0500

    feat: add CORS support to allow cross-origin requests

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const YAML = require('yamljs');
 const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
 
 require('./config/db');
 
 const swaggerDocument = YAML.load('./docs/swagger.yaml');
 
+app.use(cors()); 
 app.use('/putPets-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 
