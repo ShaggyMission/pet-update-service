0a6d6847a13065a9ca755792c2f55a8d0a0b11d0
Lis Pacheco <lmpacheco@uce.edu.ec>
2025-07-05 13:42:54 -0500

    fix: update MongoDB connection string to new IP and credentials

--- config/db.js
+++ config/db.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 
 if (process.argv.join().indexOf('jest') === -1) {
-  const uri = 'mongodb+srv://lmpacheco:ZrvwTwBSagITmKAT@cluster0.ny218yj.mongodb.net/shaggymission_pets?retryWrites=true&w=majority&appName=Cluster0';
+  const uri = 'mongodb://admin:Lis12345@54.156.27.209:27017/admin';
 
-  mongoose.connect(uri)
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.error('MongoDB connection error:', err));
+  mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 }
 
 module.exports = mongoose;
+
 
